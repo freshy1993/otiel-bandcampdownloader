5879c33e638fe7b7d05e43239e11604ed21cfc12
Otiel <Otiel@users.noreply.github.com>
2018-11-26 13:41:32 +0100

    feat: add first layer for dark theme implementation
    Based on https://michaelscodingspot.com/2017/06/30/wpf-complete-guide-themes-skins and https://github.com/DanPristupov/WpfExpressionBlendTheme

--- Sources/BandcampDownloader/App.xaml.cs
+++ Sources/BandcampDownloader/App.xaml.cs
@@ -2,9 +2,12 @@
 
 namespace BandcampDownloader {
 
+    public enum Skin { Dark, Light }
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App: Application {
+        public static Skin Skin { get; set; } = Skin.Dark;
     }
 }
---
+++ Sources/BandcampDownloader/Themes/SkinResourceDictionary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+
+namespace BandcampDownloader {
+
+    public class SkinResourceDictionary: ResourceDictionary {
+        private Uri darkSource;
+
+        public Uri DarkSource {
+            get {
+                return darkSource;
+            }
+            set {
+                darkSource = value;
+                UpdateSource();
+            }
+        }
+
+        private void UpdateSource() {
+            switch (App.Skin) {
+                case Skin.Dark:
+                    if (DarkSource != null) {
+                        base.Source = DarkSource;
+                    }
+                    break;
+                case Skin.Light:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
